65091ac4e7119bae8e7fbe1a3bdee1377b617884
kjac <kja@umbraco.dk>
2024-03-10 14:35:18 +0100

    A little clean-up

--- src/Site/astro.config.mjs
+++ src/Site/astro.config.mjs
@@ -1,5 +1,7 @@
 import { defineConfig } from 'astro/config';
 import { loadEnv } from "vite";
+
+// need to prevent Node.js from rejecting self-signed certificates in development mode - see .env.development
 const { NODE_TLS_REJECT_UNAUTHORIZED } = loadEnv(process.env.NODE_ENV, process.cwd(), "");
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = NODE_TLS_REJECT_UNAUTHORIZED;
 
