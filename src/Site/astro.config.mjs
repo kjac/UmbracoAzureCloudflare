279917021d2ed125143c785ed0a1adad143757c9
kjac <kja@umbraco.dk>
2024-03-17 15:11:52 +0100

    Setup preview environment

--- src/Site/astro.config.mjs
+++ src/Site/astro.config.mjs
@@ -1,11 +1,24 @@
 import { defineConfig } from 'astro/config';
 import { loadEnv } from "vite";
+import cloudflare from "@astrojs/cloudflare";
+import node from "@astrojs/node";
+
+const { NODE_TLS_REJECT_UNAUTHORIZED, USE_SSR, SSR_ADAPTER } = loadEnv(process.env.NODE_ENV, process.cwd(), "");
 
 // need to prevent Node.js from rejecting self-signed certificates in development mode - see .env.development
-const { NODE_TLS_REJECT_UNAUTHORIZED } = loadEnv(process.env.NODE_ENV, process.cwd(), "");
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = NODE_TLS_REJECT_UNAUTHORIZED;
 
+// construct the Astro config based on the configured environment variables
+const config = {
+  output: USE_SSR
+      ? 'server'
+      : 'static'
+};
+if (USE_SSR) {
+  config.adapter = SSR_ADAPTER === 'Node'
+      ? config.adapter = node({ mode: 'standalone' })
+      : cloudflare();
+}
+
 // https://astro.build/config
-export default defineConfig({
-    output: 'static'
-});
+export default defineConfig(config);
